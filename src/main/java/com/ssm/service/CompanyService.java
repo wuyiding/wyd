01bbda4b8f8bb9fc50f20e395da4fb2630847de2
wuyd <707999813@qq.com>
2017-04-08 09:16:44 +0800

    spring-mybatis

--- src/main/java/com/ssm/service/CompanyService.java
+++ src/main/java/com/ssm/service/CompanyService.java
@@ -2,16 +2,12 @@ package com.ssm.service;
 import com.ssm.mapper.CompanyMapper;
 import com.ssm.model.Company;
 import com.ssm.model.ResponseData;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.InputStream;
+
 import java.util.List;
 
 /**
@@ -21,20 +17,15 @@ import java.util.List;
 @RequestMapping("/company")
 public class CompanyService {
 
-    private static SqlSessionFactory sessionFactory;
-    private static SqlSession session;
+//    private static SqlSessionFactory sessionFactory;
+//    private static SqlSession session;
     private static ApplicationContext ctx;
     private static CompanyMapper companyMapper;
     static {
-        String mybatisResource = "mybatis.xml";
-
-        /* 使用类加载器加载mybatis的配置文件（它也加载关联的映射文件） */
-        try{
-            InputStream ins = Resources.getResourceAsStream(mybatisResource);
-            //构建sqlSession的工厂
-            sessionFactory = new SqlSessionFactoryBuilder().build(ins);
-            //创建能执行映射文件中sql的sqlSession
-            session = sessionFactory.openSession();
+        // spring-mybatis
+        try {
+            ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+            companyMapper = (CompanyMapper) ctx.getBean("companyMapper");
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -43,51 +34,27 @@ public class CompanyService {
     @RequestMapping(value = "{username}", method = RequestMethod.GET)
     public @ResponseBody Company getCompany(@PathVariable String username) {
 
-
-        /**
-         * 映射sql的标识字符串
-         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
-         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
-         */
-//        String statement = "com.vstudio.mapping.Teacher.selectTeacherByID";//映射sql的标识字符串
-//        Teacher teacher = session.selectOne(statement, 8000);
-
-        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
+//        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
         Company company = companyMapper.selectCompanyByUsername(username);
-
         return company;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody List<Company> getAllCompany() {
-
-//        try {
-//            ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml");
-//            teacherMapper = (TeacherMapper) ctx.getBean("teacherMapper");
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        }
-        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
+    public @ResponseBody List<Company> getAllCompanies() {
 
         List<Company> companies = companyMapper.selectCompanies();
-
         return companies;
     }
     @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody ResponseData addTeacher(@RequestBody Company company) {
+    public @ResponseBody ResponseData addCompany(@RequestBody Company company) {
         ResponseData responseData = new ResponseData();
-
-        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
         companyMapper.addCompany(company);
-        session.commit();
         responseData.setSuccess(true);
         return responseData;
     }
     @RequestMapping(value = "{username}", method = RequestMethod.PUT)
     public @ResponseBody ResponseData updateCompany(@RequestBody Company company, @PathVariable String username) {
         ResponseData responseData = new ResponseData();
-
-        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
         Company companyForUpdate = companyMapper.selectCompanyByUsername(username);
         companyForUpdate.setCompanyname(company.getCompanyname());
         companyForUpdate.setStarttime(company.getStarttime());
@@ -101,20 +68,17 @@ public class CompanyService {
 
         // to finish all setter...
         companyMapper.updateCompany(companyForUpdate);
-        session.commit();
+
 
         responseData.setSuccess(true);
         return responseData;
     }
     @RequestMapping(value = "{username}", method = RequestMethod.DELETE)
      public @ResponseBody ResponseData deleteCompany(@PathVariable String username) {
-                ResponseData responseData = new ResponseData();
+        ResponseData responseData = new ResponseData();
+        companyMapper.deleteCompany(username);
 
-                        CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
-                companyMapper.deleteCompany(username);
-                session.commit();
-
-                        responseData.setSuccess(true);
+        responseData.setSuccess(true);
                return responseData;
     }
 
