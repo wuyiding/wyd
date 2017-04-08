592800de949b60f3c0be5da2d3175785c1886863
wuyd <707999813@qq.com>
2017-04-08 09:19:08 +0800

    spring-mybatis1

--- src/main/java/com/ssm/mapper/CompanyMapper.java
+++ src/main/java/com/ssm/mapper/CompanyMapper.java
@@ -1,6 +1,7 @@
 package com.ssm.mapper;
 
 import com.ssm.model.Company;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -8,10 +9,15 @@ import java.util.List;
  * Created by wuyd on 2017/4/6.
  */
 public interface CompanyMapper {
+    @Autowired
     public Company selectCompanyByUsername(String username);
+    @Autowired
     public List<Company> selectCompanies();
+    @Autowired
     public void addCompany(Company company);
+    @Autowired
     public void updateCompany(Company company);
+    @Autowired
     public void deleteCompany(String username);
 
 }
